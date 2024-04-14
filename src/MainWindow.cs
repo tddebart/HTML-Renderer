eb943287d3f3d67544e1a85675aed3dd6c31fa79
tddebart <tddebart@gmail.com>
2024-04-14 15:36:19 +0200

    Initial commit

--- src/MainWindow.cs
+++ src/MainWindow.cs
@@ -1,101 +1,71 @@
-﻿using System.Runtime.InteropServices;
-using ImGuiGeneral;
-using ImGuiNET;
-using static SDL2.SDL;
-using static SDL2.SDL_ttf;
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
 
 public class MainWindow
 {
-	public IntPtr window; 
-	public IntPtr gl_context;
-	private IntPtr sdlRenderer;
-	public ImGuiGLRenderer imGuiRenderer;
-	private IntPtr font;
-	
-	[DllImport("user32.dll", SetLastError=true)]
-	static extern bool SetProcessDPIAware();
-	
-	public bool quit = false;
-
-	private int textSize = 24;
-	private int direction = 1;
+	private readonly RenderWindow window;
 
 	public MainWindow()
 	{
-		SetProcessDPIAware();
-		(window, gl_context) = ImGuiGL.CreateWindowAndGLContext("My SDL Imgui window", 1280, 720);
-		imGuiRenderer = new ImGuiGLRenderer(window, gl_context);
-		sdlRenderer = SDL_CreateRenderer(window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+		// Create a window
+		window = new RenderWindow(new VideoMode(1280, 720), "SFML Box with Text Example");
+		window.SetVerticalSyncEnabled(true);
 
-		TTF_Init();
-		
-		while (!quit)
+		// Load a font
+		var font = new Font("Ubuntu-Regular.ttf");
+
+		// Create a rectangle shape (box)
+		var box = new RectangleShape(new Vector2f(200, 100))
 		{
-			Update();
-		}
-		//
-		// SDL_DestroyRenderer(imGuiRenderer);
-		SDL_GL_DeleteContext(gl_context);
-		SDL_DestroyWindow(window);
-		TTF_Quit();
-		SDL_Quit();
-	}
+			Position = new Vector2f(350, 250),
+			FillColor = Color.Red
+		};
 
-	public void Update()
-	{
-		EventHandling();
-		
-		
-		
-		SDL_SetRenderDrawColor(sdlRenderer, 255, 255, 255, 255);
-		SDL_RenderClear(sdlRenderer);
-
-		// Render SDL text using SDL_ttf
-		SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255); // Set text color to black
-		IntPtr surface = TTF_RenderText_Solid(font, "Hello, SDL_ttf!", new SDL_Color { r = 0, g = 255, b = 0, a = 255 });
-		IntPtr texture = SDL_CreateTextureFromSurface(sdlRenderer, surface);
-		SDL_FreeSurface(surface);
-
-		SDL_Rect dstrect = new SDL_Rect { x = 100, y = 100, w = 200, h = 200 }; // Adjust size as needed
-		SDL_RenderCopy(sdlRenderer, texture, IntPtr.Zero, ref dstrect);
-
-		SDL_DestroyTexture(texture);
-
-		// Start a new frame for ImGui
-		imGuiRenderer.NewFrame();
-
-		// Render ImGui content
-		ImGui.Text("Hello, ImGui!");
-
-		// Perform your game's rendering then render the ImGui data
-		imGuiRenderer.Render();
-
-		// Swap SDL window buffers to display the rendered ImGui on top of your SDL text
-		SDL_GL_SwapWindow(window);
-		
-		SDL_Delay(10);
-	}
-
-	private void EventHandling()
-	{
-		while (SDL_PollEvent(out var _event) != 0)
+		// Create a text object
+		var text = new Text(
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\nmolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\nnumquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\noptio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\nobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\nnihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\ntenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\nquia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \nsapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\nrecusandae alias error harum maxime adipisci amet laborum. Perspiciatis \nminima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit \nquibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur \nfugiat, temporibus enim commodi iusto libero magni deleniti quod quam \nconsequuntur! Commodi minima excepturi repudiandae velit hic maxime\ndoloremque. Quaerat provident commodi consectetur veniam similique ad \nearum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo \nfugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore \nsuscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium\nmodi minima sunt esse temporibus sint culpa, recusandae aliquam numquam \ntotam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam \nquasi aliquam eligendi, placeat qui corporis!",
+			font)
 		{
-			imGuiRenderer.ProcessEvent(_event);
-			switch (_event.type)
-			{
-				case SDL_EventType.SDL_QUIT:
-					quit = true;
-					break;
-        			
-				case SDL_EventType.SDL_KEYDOWN:
-					if (_event.key.keysym.sym == SDL_Keycode.SDLK_ESCAPE)
-					{
-						quit = true;
-					}
+			Position = new Vector2f(360, 260),
+			CharacterSize = 24,
+			FillColor = Color.White
+		};
 
-					break;
-			}
+		window.Closed += (_, _) => { window.Close(); };
+
+		window.Resized += (sender, e) =>
+		{
+			// Adjust the view to the new size of the window
+			window.SetView(new View(new FloatRect(0, 0, e.Width, e.Height)));
+		};
+
+		float baseSize = 24; // Base size of the text
+		var pulsateSpeed = 4f; // Speed of the pulsating effect
+		var pulsateScale = 1f; // Scale of the pulsating effect
+
+		var clock = new Clock();
+		// Main loop
+		while (window.IsOpen)
+		{
+			// Process events
+			window.DispatchEvents();
+
+			// Clear the window
+			window.Clear(Color.Black);
+
+			// Draw the box
+			window.Draw(box);
+
+			var elapsedTime = clock.ElapsedTime.AsSeconds();
+			var pulsateValue = (float)(Math.Sin(elapsedTime * pulsateSpeed) * pulsateScale + 1);
+			text.CharacterSize = (uint)(baseSize * pulsateValue);
+
+			// Draw the text
+			window.Draw(text);
+
+			// Display the window
+			window.Display();
 		}
 	}
 }
-
